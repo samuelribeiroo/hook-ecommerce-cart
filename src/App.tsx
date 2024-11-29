1619502ae0f7147545671b1a43b37321365010ff
samuelribeiroo <samuelribeirodev@gmail.com>
2024-11-28 22:28:05 -0300

    feat(route): application routed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./index.css"
-import Page from "./page/page"
+import Cart from "./components/Cart/cart";
+import "./index.css";
+import NotFoundPage from "./page/not-found";
+import Page from "./page/page";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
-	return <Page />
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Page />} />
+				<Route path="/cart" element={<Cart />} />
+				<Route path="*" element={<NotFoundPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
